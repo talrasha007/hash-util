cc2073bd9104241dfba836984500ee20ace811f2
Anlv.Tan <anlv.tan@tendcloud.com>
2014-10-29 15:30:20 +0800

    sortable hash & geo hash support.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var crypto = require('crypto');
+var crypto = require('crypto'),
+    sh = require('sortable-hash');
 
 function createHash(str, alg, encoding) {
     return crypto.createHash(alg).update(str, 'utf8').digest(encoding || 'hex');
@@ -18,4 +19,23 @@ exports.sha256 = function (str, encoding) {
 
 exports.sha512 = function (str, encoding) {
     return createHash(str, 'sha512', encoding);
+};
+
+exports.sortable = sh;
+
+exports.geo = {
+    decode: function (hash) {
+        var decoded = sh.decode(hash, 2);
+        return {
+            lng: decoded[0] * 180 / 100,
+            lat: decoded[1] * 90 / 100
+        }
+    },
+
+    encode: function (coord, precision) {
+        return sh.encode([
+            (coord.lng || coord.longitude) * 100 / 180,
+            (coord.lat || coord.latitude) * 100 / 90
+        ], precision);
+    }
 };
